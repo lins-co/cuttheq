3d1dc423bef658864917b5a487ca914af740286f
Your Name <yourname@example.com>
2024-10-21 16:28:59 +0530

    hello

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`); 
